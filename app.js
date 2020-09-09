560d8dd37c3e121ca9b83748dab84f6837cb9455
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-09-09 19:28:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./main.jpg",
+  image2: "./main.jpg",
+  angle: Math.PI / 4,
+  displacementImage: "./effect3.png",
+});
